fed65021679054e9814f0d7bc8199fff8fee9703
lukelegary <50061173+lukelegary@users.noreply.github.com>
2023-10-11 20:36:42 +0000

    Wed, Oct 11, 2023, 5:36 PM -03:00

---
+++ 00-Python Object and Data Structure Basics/01-variable-assignment.py
@@ -0,0 +1,12 @@
+a = 10
+# If the process below is made again and again, it will get
+# the last value assignmenttype/ for 'a' and, in this cae, run a addition
+a = a + a
+print(a)
+
+type(a)
+
+a = 30.1
+type(a)
+
+int = 4
