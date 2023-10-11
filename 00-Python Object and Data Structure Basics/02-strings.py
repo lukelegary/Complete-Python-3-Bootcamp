d578bc3d358841ac1eb458e905c5e48ee1475c79
lukelegary <50061173+lukelegary@users.noreply.github.com>
2023-10-11 21:02:33 +0000

    Wed, Oct 11, 2023, 6:02 PM -03:00

--- 00-Python Object and Data Structure Basics/02-strings.py
+++ 00-Python Object and Data Structure Basics/02-strings.py
@@ -1,20 +1,29 @@
 print("hello world")
-print("hello \nworld")
-print("hello \tworld")
-print("hello \n\nworld")
-print("hello \t\tworld")
+  print("hello \nworld")
+  print("hello \tworld")
+    print("hello \n\nworld")
+    print("hello \t\tworld")
 
 len("hello")
 
 mystring = "Hello World"
-mystring
-mystring[0]
-mystring[8]
-mystring[-2]
-mystring[-5]
+  mystring
+    mystring[0]
+    mystring[8]
+    mystring[-2]
+    mystring[-5]
 
 mystring = "fwegewrfca"
-mystring = [2]
+  mystring
+    mystring[2]
+    mystring[2:]
+    mystring[:3]
+      mystring[4:6]
 
+      mystring[::]
+      mystring[::4]
+
+# the ':' says: "go up the character, but no include it
+# it works either from beginning to end, and end to beginning
 
 
